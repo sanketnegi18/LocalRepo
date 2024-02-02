604fa411a0860211c76abfcb6ee8b8f6c65fffc4
Sanket Singh Negi <sanketnegi18@gmail.com>
2024-02-02 13:51:49 +0530

    add new lines

--- Number_Plate.py
+++ Number_Plate.py
@@ -1,20 +1,23 @@
 import cv2
+import os
 
-harcascade = "model/haarcascade_indian_plate_number.xml"
+harcascade = "model/haarcascade_russian_plate_number.xml"
 
 cap = cv2.VideoCapture(0)
 
-cap.set(3,640) # width
-cap.set(4,480) # height
+cap.set(3, 640) # width
+cap.set(4, 480) #height
 
 min_area = 500
-count = 0
+folder_path = '.\\plates'
+files = os.listdir(folder_path)
+count = int(len(files))
 
 while True:
     success, img = cap.read()
 
     plate_cascade = cv2.CascadeClassifier(harcascade)
-    img_gray = cv2.cvtcolor(img, cv2.color_BGR2GRAY)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
 
@@ -22,20 +25,20 @@ while True:
         area = w * h
 
         if area > min_area:
-            cv2.rectangle(img, (x,y),(x+w, y+h),(0,255,0), 2)
-            cv2.putText(img, "Number Plate", (x,y-5),cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0,255), 2)
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(img, "Number Plate", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
 
             img_roi = img[y: y+h, x:x+w]
             cv2.imshow("ROI", img_roi)
 
 
+    
+    cv2.imshow("Result", img)
 
-            cv2.imshow("Result", img)
-
-            if cv2.waitkey(1) & 0xFF == ord('s'):
-                cv2.imwrite("plates/scaned_img_" + str(count) + ".jpg", img_roi)
-                cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
-                cv2.putText(img, "Plste Saved", (150, 265),cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
-                cv2.imshow("Results",img)
-                cv2.waitkey(500)
-                count += 1
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("plates/scaned_img_" + str(count) + ".jpg", img_roi)
+        cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+        cv2.imshow("Results",img)
+        cv2.waitKey(500)
+        count += 1
